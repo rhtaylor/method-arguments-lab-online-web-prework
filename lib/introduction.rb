ab5e7a2aad7cc7503874145f09eeb0ff2efd1835
Randall Taylor <randyisbusy@gmail.com>
2019-07-13 18:13:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,8 @@ def introduction(name)
   phrase = "Hi, my name is #{name}."
   puts phrase
 end
+
+def introduction_with_language(name, language)
+  phrase = "Hi, my name is #{name} and I am learing to program in #{language.}"
+  puts phrase
+end
